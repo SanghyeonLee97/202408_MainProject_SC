894c38d8937d094599342d06a006161bc61b9d65
tldn0631 <tldn0631@gmail.com>
2024-08-30 12:58:04 +0900

    프로젝트 생성,마이바티스 설정,테스트 완료

---
+++ src/main/java/com/spring/biz/DTO.java
@@ -0,0 +1,19 @@
+package com.spring.biz;
+
+//mybatis 연결 테스트용 DTO
+public class DTO {
+	int id;
+	String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ src/main/java/com/spring/biz/Test.java
@@ -0,0 +1,54 @@
+package com.spring.biz;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+//마이바티스 작동 테스트용 컨트롤러
+
+@Controller
+public class Test {
+	@Autowired
+	SqlSession ss; 
+	
+	@RequestMapping(value="/select.do")
+	public String select() {
+		List<DTO> test = ss.selectList("testName.testSelect");
+		for(int i=0;i<test.size();i++) {
+			System.out.println(test.get(i).getId()+" "+test.get(i).getName());
+		}
+		return "home";
+	}
+	
+	@RequestMapping(value="/insert.do")
+	public String insert() {
+		String name = "홍길동";
+		ss.selectList("testName.testInsert", name);
+		return "home";
+	}
+	
+	@RequestMapping(value="/update.do")
+	public String update() {
+		String name = "차길동";
+		int id = 5;
+		HashMap<String, Object> Map = new HashMap<>();
+		
+		Map.put("id", id);
+		Map.put("name", name);
+		ss.selectList("testName.testUpdate", Map);
+		return "home";
+	}
+	
+	@RequestMapping(value="/delete.do")
+	public String delete() {
+		int id = 5;
+		ss.selectList("testName.testDelete",id);
+		return "home";
+	}
+
+}
