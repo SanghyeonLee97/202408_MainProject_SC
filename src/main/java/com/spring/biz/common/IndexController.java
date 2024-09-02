1020a886e887b5052762704d3870d5c0678461e2
tldn0631 <tldn0631@gmail.com>
2024-09-02 17:24:42 +0900

    index 경로변경,index 호출 컨트롤러 생성,세팅

--- src/main/java/com/spring/biz/common/IndexController.java
+++ src/main/java/com/spring/biz/common/IndexController.java
@@ -1,13 +1,20 @@
-//package com.spring.biz.common;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-//@Controller
-//public class IndexController {
-//	@GetMapping("/")
-//    public String home() {
-//		System.out.println("test");
-//        return "index";
-//    }
-//}
+package com.spring.biz.common;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+    @RequestMapping("/index")
+    public String connectIndex(HttpServletResponse response) {
+    	response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.setHeader("Pragma", "no-cache");
+        response.setDateHeader("Expires", 0);
+        
+        System.out.println("Test"); // 콘솔에 "Test" 출력
+        return "index"; // index.jsp로 이동
+    }
+}
