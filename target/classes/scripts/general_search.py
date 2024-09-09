231104add699389b861f76932c1c8a1de204b695
lkh001 <khlmind@naver.com>
2024-09-09 12:43:10 +0900

    검색 조건 수정

--- target/classes/scripts/general_search.py
+++ target/classes/scripts/general_search.py
@@ -68,8 +68,7 @@ elif searchTxt.strip() != '' and keywordTxt.strip() == ''  :
         ]           
     }
     
-    flag = "2"    
-
+    flag = "2"  
     
 # 검색어 입력 후 & wifi, anientry, parking, wheelchair, playroom, smokingroom 선택 했을 경우   
 elif searchTxt.strip() != '' and amenities.strip() != '' :
@@ -101,30 +100,89 @@ elif searchTxt.strip() != '' and amenities.strip() != '' :
     } 
     
     flag = "3"
-
     
-# 검색어 입력 후 & 좋아요수, 리뷰수, 여자 좋아요, 자 좋아요 선택 했을 경우    
-elif searchTxt.strip() != '' and etc_like_cnt.strip() != '' :     
+# 검색어 없고 & wifi, anientry, parking, wheelchair, playroom, smokingroom 선택 했을 경우      
+elif searchTxt.strip() == '' and amenities.strip() != '' :
     
     query = {
         "query": {
-          "match": {
-            "cafe_name.nori": searchTxt
+          "bool": {
+            "must": [
+              {
+                "match": {
+                  amenities: "Y"
+                }
+              }
+            ]
           }
         },
         "sort": [
             {
-                etc_like_cnt: {
-                    "order": "desc"
+                "cafe_id": {
+                    "order": "asc"
                 }
             }
-        ]
-    }
+        ]      
+    } 
     
     flag = "4"
-
     
-# 검색어 입력 후 & 분위기 : '가성비 좋은', '고급스러운', '예쁜', '격식있는', '이색적인' 선택 했을 경우    
+# 검색어 입력 후 & 좋아요수, 리뷰수, 여자 좋아요, 자 좋아요 선택 했을 경우 수가 많은 순으로 리턴   
+elif searchTxt.strip() != '' and etc_like_cnt.strip() != '' :     
+    
+    query = {
+      "query": {
+        "bool": {
+          "must": [
+            {
+              "match": {
+                "cafe_name.nori": searchTxt
+              }
+            },
+            {
+              "range": {
+                etc_like_cnt: {
+                  "gt": 0
+                }
+              }
+            }
+          ]
+        }
+      },
+      "sort": [
+        {
+          etc_like_cnt: {
+            "order": "desc"
+          }
+        }
+      ]
+    }
+    
+    flag = "5"    
+    
+# 검색어 없고 & 좋아요수, 리뷰수, 여자 좋아요, 남자 좋아요 선택 했을 경우 수가 많은 순으로 리턴   
+elif searchTxt.strip() == '' and etc_like_cnt.strip() != '' :     
+    
+    query = {
+      "query": {
+        "range": {
+          etc_like_cnt: {
+            "gt": 0
+          }
+        }
+      },
+      "sort": [
+        {
+          etc_like_cnt: {
+            "order": "desc"
+          }
+        }
+      ]
+    }  
+    
+    flag = "6" 
+    
+# 검색어 입력 후 & 분위기 : '가성비 좋은', '고급스러운', '예쁜', '격식있는', '이색적인' 선택 했을 경우 수가 많은 순으로 리턴   
 elif searchTxt.strip() != '' and keywordTxt.strip() in ['M01', 'M02', 'M03', 'M04', 'M05'] : 
     
     query = {
@@ -140,6 +198,13 @@ elif searchTxt.strip() != '' and keywordTxt.strip() in ['M01', 'M02', 'M03', 'M0
                 "match": {
                   "mood": keywordTxt
                 }
+              },
+              {
+                "range": {
+                  "mood_count": {
+                    "gt": 0
+                  }
+                }
               }
             ]
           }
@@ -153,29 +218,95 @@ elif searchTxt.strip() != '' and keywordTxt.strip() in ['M01', 'M02', 'M03', 'M0
         ] 
     }
     
-    flag = "5"    
-
+    flag = "7"   
     
-# 검색어 입력 후 & 10대, 20대, 30대, 40대, 50대, 60대이상 좋아요 선택 했을 경우    
-elif searchTxt.strip() != '' and age_like_cnt.strip() != '' :   
+# 검색어 없고 & 분위기 : '가성비 좋은', '고급스러운', '예쁜', '격식있는', '이색적인' 선택 했을 경우 수가 많은 순으로 리턴    
+elif searchTxt.strip() == '' and keywordTxt.strip() in ['M01', 'M02', 'M03', 'M04', 'M05'] : 
     
     query = {
         "query": {
-          "match": {
-            "cafe_name.nori": searchTxt
+          "bool": {
+            "must": [
+              {
+                "match": {
+                  "mood": keywordTxt
+                }
+              },
+              {
+                "range": {
+                  "mood_count": {
+                    "gt": 0
+                  }
+                }
+              }
+            ]
           }
         },
         "sort": [
             {
-                age_like_cnt: {
+                "mood_count": {
                     "order": "desc"
                 }
             }
-        ]
+        ] 
     }
     
-    flag = "6"       
+    flag = "8"    
     
+# 검색어 입력 후 & 10대, 20대, 30대, 40대, 50대, 60대이상 좋아요 선택 했을 경우 수가 많은 순으로 리턴        
+elif searchTxt.strip() != '' and age_like_cnt.strip() != '' :   
+    
+    query = {
+      "query": {
+        "bool": {
+          "must": [
+            {
+              "match": {
+                "cafe_name.nori": searchTxt
+              }
+            },
+            {
+              "range": {
+                age_like_cnt: {
+                  "gt": 0
+                }
+              }
+            }
+          ]
+        }
+      },
+      "sort": [
+        {
+          age_like_cnt: {
+            "order": "desc"
+          }
+        }
+      ]
+    }
+    
+    flag = "9"  
+    
+# 검색어 없고 & 10대, 20대, 30대, 40대, 50대, 60대이상 좋아요 선택 했을 경우 수가 많은 순으로 리턴        
+elif searchTxt.strip() == '' and age_like_cnt.strip() != '' :   
+    
+    query = {
+      "query": {
+        "range": {
+          age_like_cnt: {
+            "gt": 0
+          }
+        }
+      },
+      "sort": [
+        {
+          age_like_cnt: {
+            "order": "desc"
+          }
+        }
+      ]
+    } 
+    
+    flag = "10"   
     
 # 검색어 입력 후 & 거리 단위 선택 했을 경우    
 elif searchTxt.strip() != '' and keywordTxt.strip() in ['100', '200', '300', '400', '500', '600', '700', '800', '900', '1000', '1100', '1200']:    
@@ -199,21 +330,45 @@ elif searchTxt.strip() != '' and keywordTxt.strip() in ['100', '200', '300', '40
         },
         "sort": [
             {
-                "mood_count": {
+                "cafe_id": {
                     "order": "desc"
                 }
             }
         ] 
     }
     
-    flag = "7"          
-         
+    flag = "11"    
+    
+# 검색어 없고 & 거리 단위 선택 했을 경우    
+elif searchTxt.strip() == '' and keywordTxt.strip() in ['100', '200', '300', '400', '500', '600', '700', '800', '900', '1000', '1100', '1200']:    
+    
+    query = {
+        "query": {
+          "bool": {
+            "must": [
+              {
+                "match": {
+                  "meter": keywordTxt
+                }
+              }
+            ]
+          }
+        },
+        "sort": [
+            {
+                "cafe_id": {
+                    "order": "desc"
+                }
+            }
+        ] 
+    }
+    
+    flag = "12"  
     
 else :    
     
-    flag = "99"
-    
-    
+    flag = "99"   
+
 
 # 검색 쿼리 실행
 response = es.search(index="cafe_index", body=query, size=500)
@@ -224,7 +379,7 @@ results = [
         "cafe_id": hit['_source'].get('cafe_id', 'N/A'),
         "cafe_name": hit['_source'].get('cafe_name', 'N/A'),
         "image_url": hit['_source'].get('image_url', 'N/A'),
-        "like_count": hit['_source'].get('like_count', 0)     
+        "like_cnt": hit['_source'].get('like_cnt', 0)     
     }
     for hit in response['hits']['hits']    
 ]
