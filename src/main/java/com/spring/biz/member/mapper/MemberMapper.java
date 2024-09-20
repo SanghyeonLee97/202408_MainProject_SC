9545aad324dc1730ef6d3661f6e20c8c9e8c771d
ekyoo117 <ekyoo117@gmail.com>
2024-09-20 14:49:48 +0900

    no message

--- src/main/java/com/spring/biz/member/mapper/MemberMapper.java
+++ src/main/java/com/spring/biz/member/mapper/MemberMapper.java
@@ -12,11 +12,12 @@ public interface MemberMapper {
 	public void addLike(MemberDTO memberDTO);
 	public int emailCheck(String email);
 	public MemberDTO login(MemberDTO memberDTO);
-	public MemberDTO getMemberInfo(int id);
+	public MemberDTO getMemberInfo(int member_id);
 	public void updateInfo(MemberDTO memberDTO);
 	public void updateLike(MemberDTO memberDTO);
 	public void deleteAccount(int member_id);
 	public void deleteType(int member_id);
+	public void deleteAct(int member_id);
 	public MemberDTO findEmail(MemberDTO memberDTO);
 	public MemberDTO findpw(MemberDTO memberDTO);
 }
